e76c474bc0cb0898aa2e05193c43f332ee78b257
Dave Smith <getdavemail@gmail.com>
2020-01-23 11:44:13 +0000

    Fix bug whereby editing state was no disabled follow successful onChange

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -305,6 +305,7 @@ function LinkControl( {
 											url: newPage.link,
 											type: newPage.type,
 										} );
+										setIsEditingLink( false );
 									} }
 									key={ `${ suggestion.id }-${ suggestion.type }` }
 									itemProps={ buildSuggestionItemProps( suggestion, index ) }
