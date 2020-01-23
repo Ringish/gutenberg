913a2aa3171dbf9aa43e4be728fc0704f4295c23
Marek Hrabe <marekhrabe@me.com>
2020-01-23 16:00:53 +0100

    support other post types in create function

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -47,7 +47,7 @@ function NavigationLinkEdit( {
 	setAttributes,
 	showSubmenuIcon,
 	insertLinkBlock,
-	savePage,
+	saveEntityRecord,
 } ) {
 	const {
 		label,
@@ -233,12 +233,16 @@ function NavigationLinkEdit( {
 								value={ link }
 								showInitialSuggestions={ true }
 								showCreateEntity={ true }
-								createEmptyPage={ ( pageTitle ) =>
-									savePage( {
-										title: pageTitle,
-										content: '',
-										status: 'publish', // TODO: use publish?
-									} )
+								createEntity={ ( type, entityTitle ) =>
+									saveEntityRecord( 'postType', type, {
+										title: entityTitle,
+										status: 'publish',
+									} ).then( ( entity ) => ( {
+										id: entity.id,
+										type,
+										title: entity.title.raw, // TODO: use raw or rendered?
+										url: entity.link,
+									} ) )
 								}
 								onChange={ ( {
 									title: newTitle = '',
@@ -322,7 +326,9 @@ export default compose( [
 		};
 	} ),
 	withDispatch( ( dispatch, ownProps, registry ) => {
+		const { saveEntityRecord } = dispatch( 'core' );
 		return {
+			saveEntityRecord,
 			insertLinkBlock() {
 				const { clientId } = ownProps;
 
@@ -337,10 +343,6 @@ export default compose( [
 				const blockToInsert = createBlock( 'core/navigation-link' );
 
 				insertBlock( blockToInsert, insertionPoint, clientId );
-			},
-			savePage( page ) {
-				const { saveEntityRecord } = dispatch( 'core' );
-				return saveEntityRecord( 'postType', 'page', page );
 			},
 		};
 	} ),
