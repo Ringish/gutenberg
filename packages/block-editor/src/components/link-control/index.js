5b6b6a17adb0b96f1b5567a3f9847ff981ccfe54
Dave Smith <getdavemail@gmail.com>
2020-01-29 09:34:22 -0800

    Remove forceUpdate
    This doesn’t actually work. Removing.

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -261,11 +261,6 @@ function LinkControl( {
 		[ handleDirectEntry, fetchSearchSuggestions ]
 	);
 
-	const forceUpdateInputValue = ( val ) => {
-		setInputValue( '' );
-		setInputValue( val );
-	};
-
 	// Render Components
 	const renderSearchResults = ( {
 		suggestionsListProps,
@@ -334,7 +329,6 @@ function LinkControl( {
 											setIsEditingLink( false );
 										} else {
 											setIsEditingLink( true );
-											forceUpdateInputValue( inputValue );
 										}
 									} }
 									key={ `${ suggestion.id }-${ suggestion.type }` }
