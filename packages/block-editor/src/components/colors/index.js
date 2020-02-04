83141cf75c9314311b696e7c8395becc86c68591
retrofox <rdsuarez@gmail.com>
2020-02-04 04:05:25 -0800

    block-editor: remove colors helper function

--- packages/block-editor/src/components/colors/index.js
+++ packages/block-editor/src/components/colors/index.js
@@ -2,7 +2,6 @@ export {
 	getColorClassName,
 	getColorObjectByAttributeValues,
 	getColorObjectByColorValue,
-	getColorObjectByColorSlug,
 } from './utils';
 export { createCustomColorsHOC, default as withColors } from './with-colors';
 export { default as __experimentalUseColors } from './use-colors';
